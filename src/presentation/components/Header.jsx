1b51391dea87f77daa70a499ae25ff27f0eb29ba
ihugowi <orushugow@gmail.com>
2025-04-28 02:25:56 -0300

    Incio da criação da SideBar

--- src/presentation/components/Header.jsx
+++ src/presentation/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import styles from './Header.module.css'
-import {EstudAI, EstudAINav} from './EstudAI';
+import { EstudAINav } from './EstudAI';
 
 const Header = ({ onMenuClick }) => {
 
