8995bdca9c0316905d9a501340472875accf326a
ihugowi <orushugow@gmail.com>
2025-05-06 15:40:52 -0300

    fix: Remoção temporária do sidebar para criação de um melhor

--- src/presentation/components/SideBar.jsx
+++ src/presentation/components/SideBar.jsx
@@ -1,25 +1,9 @@
-const SideBar = ({ onClose, isOpen }) => {
+import React from 'react'
+
+const SideBar = () => {
   return (
-    <div className={`fixed top-0 right-0 w-56 h-screen bg-zinc-900 text-white p-5 z-50 transition-transform duration-300 ease-in-out ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
-      <button onClick={onClose} className="cursor-pointer hover:text-amber-800">
-        <i className="fa-solid fa-xmark"></i>
-      </button>
-      <nav className="mt-6">
-        <ul>
-          <li className="p-1.5 flex items-center gap-2 text-base cursor-pointer text-amber-200">
-            <i className="!text-amber-200 bi bi-house-fill "></i>
-            <span>Home</span>
-          </li>
-          <li>
+    <div>SideBar</div>
+  )
+}
 
-          </li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </nav>
-    </div>
-  );
-};
-
-export default SideBar;
+export default SideBar
--- src/presentation/layouts/LayoutPrincipal.jsx
+++ src/presentation/layouts/LayoutPrincipal.jsx
@@ -1,19 +1,17 @@
 import { Outlet } from "react-router-dom";
-import { useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import SideBar from "../components/SideBar";
 
 const LayoutPrincipal = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <>
-      <Header onMenuClick={() => setIsMenuOpen(true)} />
+      <Header />
       <main>
         <Outlet />
       </main>
       <Footer />
-      <SideBar isOpen={isMenuOpen} onClose={() => setIsMenuOpen(false)} />
+      <SideBar />
     </>
   );
 };
