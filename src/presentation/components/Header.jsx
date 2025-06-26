752b2aca2be35b8a5181c030b15c416b16712ca5
ihugow <orushugow@gmail.com>
2025-06-26 00:43:59 -0300

    fix: Ajustes no header

--- src/presentation/components/Header.jsx
+++ src/presentation/components/Header.jsx
@@ -41,16 +41,9 @@ const Header = () => {
     >
       <div
         id="container"
-        className="flex items-center justify-between mx-auto px-4 py-2"
+        className="flex items-center justify-between h-full mx-auto px-4"
       >
         <EstudAI />
-        <div id="ways" className="">
-          <ul className="hidden gap-10 text-white font-semibold text-lg overflow-hidden md:flex">
-            <li>Explorar</li>
-            <li>Comunidade</li>
-            <li>Sobre nós</li>
-          </ul>
-        </div>
 
         <div id="right_buttons" className="flex items-center gap-2">
           <button className="flex items-center justify-center p-1 text-white rounded-md bg-[#10151f] cursor-pointer border-[1.5px] border-[#292d41] duration-300 hover:bg-[#292d41]">
@@ -58,13 +51,13 @@ const Header = () => {
           </button>
 
           {user ? (
-            <div className="flex items-center gap-2 p-0.5 pl-2.5 bg-[#10151f] border-[1.5px] border-[#292d41] rounded-full">
+            <div className="flex items-center gap-2 p-0.5 pl-2.5">
               <button onClick={openModal}>
                 <FaPlus className="size-5 text-white cursor-pointer transition-colors" />
               </button>
               <DropDown
                 className="mr-4"
-                sideOffset={12}
+                sideOffset={9.5}
                 openOnHover
                 items={[
                   {
@@ -89,10 +82,38 @@ const Header = () => {
                   />
                 </button>
               </DropDown>
+              <DropDown
+                className="mr-4"
+                openOnHover
+                sideOffset={19.5}
+                items={[
+                  {
+                    label: "Seja bem-vindo(a)",
+                    icon: <FaCircle className="text-green-500 text-xs" />,
+                    onSelect: () => {},
+                  },
+                  {
+                    label: "Alerta de sistema",
+                    icon: <FaCircle className="text-yellow-500 text-xs" />,
+                    onSelect: () => {},
+                  },
+                  { type: "separator" },
+                  {
+                    label: "Ver todas",
+                    icon: <IoNotifications />,
+                    onSelect: () => navigate("/notificacoes"),
+                  },
+                ]}
+              >
+                <div className="flex items-center justify-center text-white relative rounded-md cursor-pointer">
+                  <FaCircle className="text-red-600 size-2.5 absolute top-0 right-0" />
+                  <IoNotifications className="size-5" />
+                </div>
+              </DropDown>
             </div>
           ) : (
             <button
-              className="text-white font-medium text-xs bg-gradient-to-br from-[#00E9EA] to-[#2e55e9] p-3 rounded-lg shadow-md transition-transform hover:scale-105 cursor-pointer"
+              className="text-white font-medium text-xs bg-gradient-to-br from-[#3cd1ff] to-[#0024C3] p-3 rounded-lg shadow-md transition-transform hover:scale-105 cursor-pointer"
               onClick={irParaLogin}
             >
               Entrar
